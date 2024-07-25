8521449a2584b0e4f6417b29ba086abd910e5a88
KubraAltun6 <kubra.altun06@gmail.com>
2024-07-26 00:22:54 +0300

    Can sistemi ekleme

---
+++ Sisyphus/Sisyphus/Assets/Controller/GameOver.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    
+    GameManager manager;
+    private void Start()
+    {
+       
+        manager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("enemy"))
+        {
+            manager.health -= 1;
+            Debug.Log("Çalýþýyor");
+        }
+    }
+}
